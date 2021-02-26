08d6f390ca5276987b5ef0baec47d9a1bcfca2c9
LaurentBenazet <benazetlaurent@gmail.com>
2021-02-26 01:00:09 +0100

    Added layout with links to all pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,12 @@ module.exports = {
         author: `@gatsbyjs`,
     },
     plugins: [
+        {
+            resolve: 'gatsby-plugin-typography',
+            options: {
+                pathToConfigModule: 'src/utils/typography',
+            }
+        },
         `gatsby-plugin-sass`,
         `gatsby-plugin-react-helmet`,
         {
