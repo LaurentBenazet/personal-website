fb49abffa225b3b7563c56feedaf3285e9de9fcb
LaurentBenazet <benazetlaurent@gmail.com>
2021-02-26 01:32:04 +0100

    Using graphql to get the site title

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {Link} from "gatsby"
+import {graphql, Link, useStaticQuery} from "gatsby"
 
 const ListLink = props => (
     <li style={{display: `inline-block`, marginRight: `1rem`}}>
@@ -8,11 +8,24 @@ const ListLink = props => (
 );
 
 export default function Layout({children}) {
+    const {site} = useStaticQuery(
+        graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+          }
+        }
+      }
+    `
+    );
     return (
         <div style={{margin: `3rem auto`, maxWidth: 650, padding: `0 1rem`}}>
             <header style={{marginBottom: `1.5rem`}}>
                 <Link to="/" style={{textShadow: `none`, backgroundImage: `none`}}>
-                    <h3 style={{display: `inline`}}>MySweetSite</h3>
+                    <h3 style={{display: `inline`}}>{site.siteMetadata.title}</h3>
                 </Link>
                 <ul style={{listStyle: `none`, float: `right`}}>
                     <ListLink to="/">Home</ListLink>
