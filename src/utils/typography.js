5ad2a6a11c62b8ee4c2a20687b2a120df5242bb5
LaurentBenazet <benazetlaurent@gmail.com>
2021-02-28 20:55:46 +0100

    Added files from markdown

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,18 @@
 import Typography from "typography"
-import fairyGateTheme from "typography-theme-fairy-gates"
 
-const typography = new Typography(fairyGateTheme);
+const typography = new Typography({
+    baseFontSize: "18px",
+    baseLineHeight: 1.6,
+    headerFontFamily: [
+        "Avenir Next",
+        "Helvetica Neue",
+        "Segoe UI",
+        "Helvetica",
+        "Arial",
+        "sans-serif",
+    ],
+    bodyFontFamily: ["Georgia", "serif"],
+});
 
 export const {scale, rhythm, options} = typography;
 export default typography
