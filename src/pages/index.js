6b18f06d0c328d0046b275f309925a13ba97cab1
LaurentBenazet <benazetlaurent@gmail.com>
2021-03-07 02:38:07 +0100

    Automatically create page from markdown + link from front page (gatsby tutorial completed)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,28 @@
 import React from "react"
-import {graphql} from "gatsby"
+import {graphql, Link} from "gatsby"
 import {css} from "@emotion/react"
 import {rhythm} from "../utils/typography"
 import Layout from "../components/layout"
 
 export default function Home({data}) {
-    console.log(data);
     return (
         <Layout>
             <div>
-                <h1
-                    css={css`
-            display: inline-block;
-            border-bottom: 1px solid;
-          `}
-                >
+                <h1 css={css`display: inline-block; border-bottom: 1px solid;`}>
                     Amazing Pandas Eating Things
                 </h1>
                 <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
                 {data.allMarkdownRemark.edges.map(({node}) => (
                     <div key={node.id}>
-                        <h3
-                            css={css`
-                margin-bottom: ${rhythm(1 / 4)};
-              `}
-                        >
-                            {node.frontmatter.title}{" "}
-                            <span
-                                css={css`
-                  color: #bbb;
-                `}
-                            >
-                — {node.frontmatter.date}
-              </span>
-                        </h3>
+                        <Link to={node.fields.slug}
+                              css={css`text-decoration: none; color: inherit;`}>
+                            <h3 css={css`margin-bottom: ${rhythm(1 / 4)};`}>
+                                {node.frontmatter.title}{" "}
+                                <span css={css`color: #bbb;`}>
+                                    — {node.frontmatter.date}
+                                </span>
+                            </h3>
+                        </Link>
                         <p>{node.excerpt}</p>
                     </div>
                 ))}
@@ -52,6 +41,9 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
           }
           excerpt
         }
